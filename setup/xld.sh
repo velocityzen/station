ff927494e5b25392d3a6c352f0c7256171c539ec
velocityzen <velocityzen@gmail.com>
2017-10-09 12:54:54 -0400

    + jq. updated .eslint

--- setup/xld.sh
+++ setup/xld.sh
@@ -15,13 +15,13 @@ defaults write jp.tmkk.XLD AutoQueryCDDB -int 1
 defaults write jp.tmkk.XLD AutoSetCompilation -int 1
 defaults write jp.tmkk.XLD MaxThreads -int 4
 defaults write jp.tmkk.XLD Tagging -int 1
-defaults write jp.tmkk.XLD ScalePixel -int 1200
-defaults write jp.tmkk.XLD ScaleImage -int 1
-defaults write jp.tmkk.XLD ScaleType -int 4
 defaults write jp.tmkk.XLD CompressionQuality -int 70
 defaults write jp.tmkk.XLD AutoLoadCover -int 1
 defaults write jp.tmkk.XLD AutoLoadCoverDontOverwrite -int 1
 defaults write jp.tmkk.XLD AutoLoadCoverName -string "cover.jpg folder.jpg front.jpg"
 defaults write jp.tmkk.XLD AddiTunes -int 1
 defaults write jp.tmkk.XLD EmbedImages -int 1
+defaults write jp.tmkk.XLD ScalePixel -int 1200
+defaults write jp.tmkk.XLD ScaleImage -int 1
+defaults write jp.tmkk.XLD ScaleType -int 4
 defaults write jp.tmkk.XLD 'NSToolbar Configuration XLDDiscViewToolbar' '{ "TB Default Item Identifiers" = ( "ExtractionMode", "NSToolbarFlexibleSpaceItem", "Extract", "GetMetadata", "EditMetadata", "Burn"); "TB Display Mode" = 2; "TB Icon Size Mode" = 2; "TB Is Shown" = 1; "TB Item Identifiers" = ( "GetMetadata", "NSToolbarFlexibleSpaceItem", "Extract" ); "TB Size Mode" = 1;}'

897d67ccf4d6a3b08eebc0308ad1855e8296f5cd
velocityzen <velocityzen@gmail.com>
2018-09-17 01:25:52 -0400

    + sketch, lightroom-cc. - brew cask clean

--- didSetup/adobe.sh
+++ didSetup/adobe.sh
@@ -1,4 +1,4 @@
-brew cask install adobe-photoshop-lightroom
+brew cask install adobe-lightroom-cc
 # brew cask install adobe-photoshop-cc
 # brew cask install adobe-illustrator-cc
 # brew cask install adobe-indesign-cc

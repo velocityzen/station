9b45297d30866fdb395c402a9c238453e2f3b224
velocityzen <velocityzen@gmail.com>
2020-03-12 18:37:01 -0400

    add qlcodeconfig. add suspicious-package

---
+++ setup/QLColorCode.sh
@@ -0,0 +1,9 @@
+brew install highlight
+brew cask install qlcolorcode
+
+defaults write org.n8gray.QLColorCode textEncoding UTF-16
+defaults write org.n8gray.QLColorCode webkitTextEncoding UTF-16
+defaults write org.n8gray.QLColorCode font Hasklig
+defaults write org.n8gray.QLColorCode fontSizePoints 12
+defaults write org.n8gray.QLColorCode hlTheme moria
+defaults write org.n8gray.QLColorCode extraHLFlags '-l -W'

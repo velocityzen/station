6296216175ee85f73f80493bdffa3c14b3210299
Alexey Novikov <alexey.novikov@reaktor.com>
2022-01-18 14:08:15 -0500

    Learn fish syntax fix functions

--- willSetup/fish/functions/cleanup.fish
+++ willSetup/fish/functions/cleanup.fish
@@ -1,11 +1,19 @@
 function cleanup -d "Cleanup system and user logs, empty trash"
   set -l logs ~/Library/Logs/* /Library/Logs/* /var/log/*
-  if count $logs > /dev/null
-      sudo rm -rf $logs
+  if test (count $logs) -gt 1
+      sudo rm -rf "$logs"
+      echo "Deleted logs:"
+      for log in $logs
+        echo "  $log"
+      end
   end
 
   set -l trash /Volumes/*/.Trashes ~/.Trash/* ~/.Trash/.*
-  if count $trash > /dev/null
-      sudo rm -rf $trash
+  if test (count $trash) -gt 1
+      sudo rm -rf "$trash"
+      echo "Emptied trash:"
+      for file in $trash
+        echo "  $file"
+      end
   end
 end
--- willSetup/fish/functions/g.fish
+++ willSetup/fish/functions/g.fish
@@ -1,13 +1,13 @@
-function time_since_last_commit
-  set ref (git symbolic-ref HEAD 2> /dev/null) || return
-  git log -1 --pretty=format:"%ar" | sed 's/\([0-9]*\) \(.\).*/\1\2/'
-end
-
 function g
-  if count $argv > /dev/null
-    git "$argv"
+  if not set -q $argv
+    git $argv
   else
-    echo "Last commit: (time_since_last_commit) ago"
+    if test -z (git symbolic-ref HEAD 2> /dev/null)
+      return
+    end
+
+    set -l minutes (git log -1 --pretty=format:"%ar" | sed 's/\([0-9]*\) \(.\).*/\1\2/')
+    echo "Last commit: $minutes ago"
     git status --short --branch
   end
 end

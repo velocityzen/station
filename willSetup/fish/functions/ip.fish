759fc8bd55f4ff3d23eaa7df4d8b405ed47e47fa
Alexey Novikov <alexey.novikov@reaktor.com>
2022-02-07 15:10:22 -0500

    Fix ip function

--- willSetup/fish/functions/ip.fish
+++ willSetup/fish/functions/ip.fish
@@ -1,3 +1,3 @@
 function ip -d "List all IPs"
-  ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'
+  ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, ""); print }'
 end

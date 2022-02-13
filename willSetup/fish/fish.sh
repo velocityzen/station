88fe273af92f25f0e7af5587c76a37a744da41fa
Alexey Novikov <alexey.novikov@reaktor.com>
2022-02-13 11:29:34 -0500

    fix fish install script

--- willSetup/fish/fish.sh
+++ willSetup/fish/fish.sh
@@ -1,9 +1,14 @@
 brew install fish starship
 
-echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
-chsh -s /opt/homebrew/bin/fish
+FISH_PATH=$(which fish)
+STARSHIP_PATH=$(which starship)
 
-echo "/opt/homebrew/bin/starship init fish | source" | tee -a ~/.config/fish/config.fish
+echo "$FISH_PATH" | sudo tee -a /etc/shells
+chsh -s $FISH_PATH
+
+mkdir -p ~/.config/fish/functions
+echo "set fish_greeting" | tee -a ~/.config/fish/config.fish
+echo "$STARSHIP_PATH init fish | source" | tee -a ~/.config/fish/config.fish
 echo "source ~/.aliases" | tee -a ~/.config/fish/config.fish
 echo "if test -e ~/.aliases.local
   source ~/.aliases.local
@@ -15,7 +20,7 @@ for file in $PWD/willSetup/fish/functions/*.fish; do
 done
 
 fish -c "fish_add_path /opt/homebrew/bin"
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install jorgebucaran/autopair.fish
-fisher install oh-my-fish/plugin-wifi-password
-fisher install oh-my-fish/plugin-aws
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && \
+  fisher install jorgebucaran/autopair.fish && \
+  fisher install oh-my-fish/plugin-wifi-password && \
+  fisher install oh-my-fish/plugin-aws"
